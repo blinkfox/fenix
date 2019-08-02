ee1dc7db8146d1443575230667a074bab53f7f58
blinkfox <chenjiayin1990@163.com>
2019-08-03 01:34:23 +0800

    新增两个基础类

---
+++ src/test/java/com/blinkfox/fenix/FenixApplication.java
@@ -0,0 +1,17 @@
+package com.blinkfox.fenix;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+/**
+ * 应用配置.
+ *
+ * @author blinkfox on 2019-08-03.
+ */
+@Configuration
+@EnableJpaRepositories
+public class FenixApplication {
+
+
+
+}
