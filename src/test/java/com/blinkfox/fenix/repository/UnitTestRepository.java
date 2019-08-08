ed55d12ba1fcd5dbae3901194c835311952ae502
blinkfox <chenjiayin1990@163.com>
2019-08-09 00:43:19 +0800

    新增了诸多原生 SQL 查询的示例

--- src/test/java/com/blinkfox/fenix/repository/UnitTestRepository.java
+++ src/test/java/com/blinkfox/fenix/repository/UnitTestRepository.java
@@ -31,13 +31,24 @@ public interface UnitTestRepository extends JpaRepository<User, String> {
     List<User> testEqual(@Param("user") User user, @Param("userId") String userId);
 
     /**
-     * 使用 {@link QueryFenix} 注解根据用户的实体 VO 类来查询用户信息.
+     * 使用 {@link QueryFenix} 注解使用原生 SQL 来查询用户信息.
      *
      * @param userId 用户ID
      * @return 用户信息集合
      */
     @QueryFenix(value = "UnitTestRepository.testNativeEqual", nativeQuery = true)
-    List<User> testNativeEqual(@Param("userId") String userId);
+    List<User> testNativeEqual(@Param("user") User user, @Param("userId") String userId);
+
+    /**
+     * 使用 {@link QueryFenix} 注解使用原生 SQL 来查询部分用户字段的信息.
+     *
+     * @param userId 用户ID
+     * @return 用户信息集合
+     */
+    @Query(value = "SELECT u.c_id, u.c_name, u.n_age, u.c_email, u.dt_create_time FROM t_user AS u WHERE u.c_id = :userId", nativeQuery = true)
+    // TODO 使用 QueryFenix 注解还有问题，目前只能使用原生的语法才行.
+    // @QueryFenix(value = "UnitTestRepository.testNativeEqualPartFields", nativeQuery = true)
+    List<Map<String, Object>> testNativeEqualPartFields(@Param("userId") String userId);
 
     /**
      * 使用原生 {@link Query} 注解来查询用户信息.
@@ -58,6 +69,15 @@ public interface UnitTestRepository extends JpaRepository<User, String> {
     List<User> testNotEqual(@Param("user") User user);
 
     /**
+     * 使用 {@link QueryFenix} 注解及原生 SQL 来查询用户信息.
+     *
+     * @param user 用户实体信息
+     * @return 用户信息集合
+     */
+    @QueryFenix(value = "UnitTestRepository.testNativeNotEqual", nativeQuery = true)
+    List<User> testNativeNotEqual(@Param("user") User user);
+
+    /**
      * 使用 {@link QueryFenix} 注解根据用户的实体 VO 类来查询用户信息.
      *
      * @param user 用户实体信息
@@ -65,6 +85,15 @@ public interface UnitTestRepository extends JpaRepository<User, String> {
      */
     @QueryFenix("UnitTestRepository.testLessThanEqual")
     List<User> testLessThanEqual(@Param("user") User user);
+
+    /**
+     * 使用 {@link QueryFenix} 注解及原生 SQL 来查询用户信息.
+     *
+     * @param user 用户实体信息
+     * @return 用户信息集合
+     */
+    @QueryFenix(value = "UnitTestRepository.testNativeLessThanEqual", nativeQuery = true)
+    List<User> testNativeLessThanEqual(@Param("user") User user);
 
     /**
      * 使用 {@link QueryFenix} 注解根据用户的实体 VO 类来查询用户信息.
@@ -76,6 +105,15 @@ public interface UnitTestRepository extends JpaRepository<User, String> {
     List<User> testLike(@Param("user") User user);
 
     /**
+     * 使用 {@link QueryFenix} 注解及原生 SQL 来查询用户信息.
+     *
+     * @param user 用户实体信息
+     * @return 用户信息集合
+     */
+    @QueryFenix(value = "UnitTestRepository.testNativeLike", nativeQuery = true)
+    List<User> testNativeLike(@Param("user") User user);
+
+    /**
      * 使用 {@link QueryFenix} 注解根据用户的实体 VO 类来查询用户信息.
      *
      * @param user 用户实体信息
@@ -83,6 +121,15 @@ public interface UnitTestRepository extends JpaRepository<User, String> {
      */
     @QueryFenix("UnitTestRepository.testStartsWith")
     List<User> testStartsWith(@Param("user") User user);
+
+    /**
+     * 使用 {@link QueryFenix} 注解及原生 SQL 来查询用户信息.
+     *
+     * @param user 用户实体信息
+     * @return 用户信息集合
+     */
+    @QueryFenix(value = "UnitTestRepository.testNativeStartsWith", nativeQuery = true)
+    List<User> testNativeStartsWith(@Param("user") User user);
 
     /**
      * 使用 {@link QueryFenix} 注解根据用户的实体 VO 类来查询用户信息.
@@ -94,6 +141,15 @@ public interface UnitTestRepository extends JpaRepository<User, String> {
     List<User> testEndsWith(@Param("user") User user);
 
     /**
+     * 使用 {@link QueryFenix} 注解及原生 SQL 类来查询用户信息.
+     *
+     * @param user 用户实体信息
+     * @return 用户信息集合
+     */
+    @QueryFenix(value = "UnitTestRepository.testNativeEndsWith", nativeQuery = true)
+    List<User> testNativeEndsWith(@Param("user") User user);
+
+    /**
      * 使用 {@link QueryFenix} 注解根据用户的实体 VO 类来查询用户信息.
      *
      * @param userMap 用 Map 存放的用户信息
@@ -101,6 +157,15 @@ public interface UnitTestRepository extends JpaRepository<User, String> {
      */
     @QueryFenix("UnitTestRepository.testBetween")
     List<User> testBetween(@Param("user") Map<String, Object> userMap);
+
+    /**
+     * 使用 {@link QueryFenix} 注解及原生 SQL 类来查询用户信息.
+     *
+     * @param userMap 用 Map 存放的用户信息
+     * @return 用户信息集合
+     */
+    @QueryFenix(value = "UnitTestRepository.testNativeBetween", nativeQuery = true)
+    List<User> testNativeBetween(@Param("user") Map<String, Object> userMap);
 
     /**
      * 使用 {@link QueryFenix} 注解根据用户的实体 VO 类来查询用户信息.
@@ -112,6 +177,15 @@ public interface UnitTestRepository extends JpaRepository<User, String> {
     List<User> testIn(@Param("userMap") Map<String, Object> userMap);
 
     /**
+     * 使用 {@link QueryFenix} 注解及原生 SQL 类来查询用户信息.
+     *
+     * @param userMap 用 Map 存放的用户信息
+     * @return 用户信息集合
+     */
+    @QueryFenix(value = "UnitTestRepository.testNativeIn", nativeQuery = true)
+    List<User> testNativeIn(@Param("userMap") Map<String, Object> userMap);
+
+    /**
      * 使用 {@link QueryFenix} 注解根据用户的实体 VO 类来查询用户信息.
      *
      * @param user 用户实体信息
@@ -119,6 +193,15 @@ public interface UnitTestRepository extends JpaRepository<User, String> {
      */
     @QueryFenix("UnitTestRepository.testIsNull")
     List<User> testIsNull(@Param("user") User user);
+
+    /**
+     * 使用 {@link QueryFenix} 注解原生 SQL 类来查询用户信息.
+     *
+     * @param user 用户实体信息
+     * @return 用户信息集合
+     */
+    @QueryFenix(value = "UnitTestRepository.testNativeIsNull", nativeQuery = true)
+    List<User> testNativeIsNull(@Param("user") User user);
 
     /**
      * 使用 {@link QueryFenix} 注解根据用户的实体 VO 类来查询用户信息.
@@ -139,7 +222,7 @@ public interface UnitTestRepository extends JpaRepository<User, String> {
     List<User> testImport(@Param("userMap") Map<String, Object> userMap, @Param("user") User user);
 
     /**
-     * 使用 {@link QueryFenix} 注解根据用户的实体 VO 类来查询用户信息.
+     * 使用 {@link QueryFenix} 注解根据用户的实体 VO 类来更新用户信息.
      *
      * @param user 用户实体信息
      * @return 用户信息集合
@@ -150,9 +233,23 @@ public interface UnitTestRepository extends JpaRepository<User, String> {
     int testSet(@Param("user") User user);
 
     /**
+     * 使用 {@link QueryFenix} 注解及原生 SQL 类来更新用户信息.
+     *
+     * @param user 用户实体信息
+     * @return 用户信息集合
+     */
+    @Transactional
+    @Modifying
+    @QueryFenix(value = "UnitTestRepository.testNativeSet", nativeQuery = true)
+    int testNativeSet(@Param("user") User user);
+
+    /**
      * 使用原生的 JPA {@link org.springframework.data.jpa.repository.Query} 的注解的执行更新.
      *
      * @param name 名称
+     * @param email 邮箱
+     * @param age 年龄
+     * @param sex 性别
      * @return 用户信息集合
      */
     @Transactional
@@ -160,5 +257,19 @@ public interface UnitTestRepository extends JpaRepository<User, String> {
     @Query("UPDATE User SET name = :name, email = :email, age = :age, sex = :sex WHERE id = '10'")
     int testUpdate(@Param("name") String name, @Param("email") String email,
                    @Param("age") int age, @Param("sex") String sex);
+
+    /**
+     * 使用原生 SQL {@link org.springframework.data.jpa.repository.Query} 来执行更新.
+     *
+     * @param name 名称
+     * @param email 邮箱
+     * @param sex 性别
+     * @return 用户信息集合
+     */
+    @Transactional
+    @Modifying
+    @Query(value = "UPDATE t_user SET c_name = :c_name, c_email = :c_email, c_sex = :c_sex WHERE c_id = '10'",
+            nativeQuery = true)
+    int testNativeUpdate(@Param("c_name") String name, @Param("c_email") String email, @Param("c_sex") String sex);
 
 }
--- src/test/java/com/blinkfox/fenix/repository/UnitTestRepositoryTest.java
+++ src/test/java/com/blinkfox/fenix/repository/UnitTestRepositoryTest.java
@@ -21,6 +21,7 @@ import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 
 import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -85,8 +86,17 @@ public class UnitTestRepositoryTest {
      */
     @Test
     public void testNativeEqual() {
-        List<User> users1 = unitTestRepository.testNativeEqual("2");
-        Assert.assertEquals(1, users1.size());
+        List<User> users = unitTestRepository.testNativeEqual(new User().setId("1"), "2");
+        Assert.assertEquals(2, users.size());
+    }
+
+    /**
+     * 测试 UnitTestRepository.testNativeEqualPartFields 中 XML SQL 的执行情况.
+     */
+    @Test
+    public void testNativeEqualPartFields() {
+        List<Map<String, Object>> users = unitTestRepository.testNativeEqualPartFields("2");
+        Assert.assertEquals(1, users.size());
     }
 
     /**
@@ -108,11 +118,30 @@ public class UnitTestRepositoryTest {
     }
 
     /**
+     * 测试 UnitTestRepository.testNativeNotEqual 中 XML SQL 的执行情况.
+     */
+    @Test
+    public void testNativeNotEqual() {
+        List<User> users = unitTestRepository
+                .testNativeNotEqual(new User().setAge(23).setStatus("0").setName("name-姓名-5"));
+        Assert.assertEquals(3, users.size());
+    }
+
+    /**
      * 测试 UnitTestRepository.testLessThanEqual 中 XML SQL 的执行情况.
      */
     @Test
     public void testLessThanEqual() {
         List<User> users = unitTestRepository.testLessThanEqual(new User().setId("1").setAge(28));
+        Assert.assertEquals(7, users.size());
+    }
+
+    /**
+     * 测试 UnitTestRepository.testNativeLessThanEqual 中 XML SQL 的执行情况.
+     */
+    @Test
+    public void testNativeLessThanEqual() {
+        List<User> users = unitTestRepository.testNativeLessThanEqual(new User().setId("1").setAge(28));
         Assert.assertEquals(7, users.size());
     }
 
@@ -126,11 +155,29 @@ public class UnitTestRepositoryTest {
     }
 
     /**
+     * 测试 UnitTestRepository.testNativeLike 中 XML SQL 的执行情况.
+     */
+    @Test
+    public void testNativeLike() {
+        List<User> users = unitTestRepository.testNativeLike(new User().setAge(25).setName("姓名").setSex("sex-性别-9"));
+        Assert.assertEquals(4, users.size());
+    }
+
+    /**
      * 测试 UnitTestRepository.testStartsWith 中 XML SQL 的执行情况.
      */
     @Test
     public void testStartsWith() {
         List<User> users = unitTestRepository.testStartsWith(new User().setName("name-姓").setSex("sex-性别"));
+        Assert.assertEquals(1, users.size());
+    }
+
+    /**
+     * 测试 UnitTestRepository.testNativeStartsWith 中 XML SQL 的执行情况.
+     */
+    @Test
+    public void testNativeStartsWith() {
+        List<User> users = unitTestRepository.testNativeStartsWith(new User().setName("name-姓").setSex("sex-性别"));
         Assert.assertEquals(1, users.size());
     }
 
@@ -144,10 +191,20 @@ public class UnitTestRepositoryTest {
     }
 
     /**
-     * 测试 UnitTestRepository.testBetween 中 XML SQL 的执行情况.
+     * 测试 UnitTestRepository.testNativeEndsWith 中 XML SQL 的执行情况.
      */
     @Test
-    public void testBetween() {
+    public void testNativeEndsWith() {
+        List<User> users = unitTestRepository.testNativeEndsWith(new User().setName("名-8").setEmail("@qq.com"));
+        Assert.assertEquals(2, users.size());
+    }
+
+    /**
+     * 构建 between 查询的上下文参数.
+     *
+     * @return map
+     */
+    private Map<String, Object> buildBetweenContext() {
         // 设置开始和结束的年龄.
         Map<String, Object> context = new HashMap<>(8);
         context.put("startAge", 22);
@@ -157,18 +214,34 @@ public class UnitTestRepositoryTest {
         Calendar c = Calendar.getInstance();
         c.set(1991, Calendar.FEBRUARY, 1);
         context.put("startBirthday", c.getTime());
-
-        Assert.assertEquals(4, unitTestRepository.testBetween(context).size());
+        return context;
     }
 
     /**
-     * 测试 UnitTestRepository.testIn 中 XML SQL 的执行情况.
+     * 测试 UnitTestRepository.testBetween 中 XML SQL 的执行情况.
      */
     @Test
-    public void testIn() {
+    public void testBetween() {
+        Assert.assertEquals(4, unitTestRepository.testBetween(this.buildBetweenContext()).size());
+    }
+
+    /**
+     * 测试 UnitTestRepository.testNativeBetween 中 XML SQL 的执行情况.
+     */
+    @Test
+    public void testNativeBetween() {
+        Assert.assertEquals(4, unitTestRepository.testNativeBetween(this.buildBetweenContext()).size());
+    }
+
+    /**
+     * 构建 in 查询的上下文参数.
+     *
+     * @return map
+     */
+    private Map<String, Object> buildInContext() {
         // 设置开始和结束的年龄.
         Map<String, Object> context = new HashMap<>(8);
-        context.put("ids", new String[] {"1", "3", "5", "7", "9"});
+        context.put("ids", new String[]{"1", "3", "5", "7", "9"});
 
         Set<String> names = new HashSet<>(4);
         names.add("name-姓名-1");
@@ -177,7 +250,23 @@ public class UnitTestRepositoryTest {
         context.put("names", names);
 
         context.put("sexs", "sex-性别-1");
-        Assert.assertEquals(2, unitTestRepository.testIn(context).size());
+        return context;
+    }
+
+    /**
+     * 测试 UnitTestRepository.testIn 中 XML SQL 的执行情况.
+     */
+    @Test
+    public void testIn() {
+        Assert.assertEquals(2, unitTestRepository.testIn(this.buildInContext()).size());
+    }
+
+    /**
+     * 测试 UnitTestRepository.testNativeIn 中 XML SQL 的执行情况.
+     */
+    @Test
+    public void testNativeIn() {
+        Assert.assertEquals(2, unitTestRepository.testNativeIn(this.buildInContext()).size());
     }
 
     /**
@@ -186,6 +275,14 @@ public class UnitTestRepositoryTest {
     @Test
     public void testIsNull() {
         Assert.assertEquals(1, unitTestRepository.testIsNull(new User().setId("4")).size());
+    }
+
+    /**
+     * 测试 UnitTestRepository.testNativeIsNull 中 XML SQL 的执行情况.
+     */
+    @Test
+    public void testNativeIsNull() {
+        Assert.assertEquals(1, unitTestRepository.testNativeIsNull(new User().setId("4")).size());
     }
 
     /**
@@ -231,6 +328,22 @@ public class UnitTestRepositoryTest {
     }
 
     /**
+     * 测试 UnitTestRepository.testNativeSet 中 XML SQL 的执行情况.
+     */
+    @Test
+    @Ignore
+    public void testNativeSet() {
+        // TODO 此处原生语法 set 还有问题.
+        String id = "10";
+        String sex = "sex-性-11";
+        Assert.assertEquals(1, unitTestRepository.testNativeSet(new User().setId(id)
+                .setName("name-姓-11").setEmail("email-11@163.com").setSex(sex)));
+        Optional<User> userOptional = unitTestRepository.findById(id);
+        Assert.assertTrue(userOptional.isPresent());
+        Assert.assertEquals(sex, userOptional.get().getSex());
+    }
+
+    /**
      * 测试 原生的 JPA {@link org.springframework.data.jpa.repository.Query} 的注解的执行情况.
      */
     @Test
@@ -239,7 +352,17 @@ public class UnitTestRepositoryTest {
         Assert.assertEquals(1, unitTestRepository.testUpdate("name-姓-11", "email-11@163.com", 31, "sex-性-11"));
         Optional<User> userOptional = unitTestRepository.findById(id);
         Assert.assertTrue(userOptional.isPresent());
-        Assert.assertEquals(31, userOptional.get().getAge());
+    }
+
+    /**
+     * 测试 原生的 JPA {@link org.springframework.data.jpa.repository.Query} 的注解的执行情况.
+     */
+    @Test
+    public void testNativeUpdate() {
+        String id = "10";
+        Assert.assertEquals(1, unitTestRepository.testNativeUpdate("name-姓-11", "email-11@163.com", "sex-性-11"));
+        Optional<User> userOptional = unitTestRepository.findById(id);
+        Assert.assertTrue(userOptional.isPresent());
     }
 
 }
