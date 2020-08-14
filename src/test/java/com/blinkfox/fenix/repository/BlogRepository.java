25cf27bfe1646518bd0dd98de7327c161da92f83
yangwenpeng <wb-ywp747121@alibaba-inc.com>
2020-08-14 18:19:08 +0800

    调整import

--- src/test/java/com/blinkfox/fenix/repository/BlogRepository.java
+++ src/test/java/com/blinkfox/fenix/repository/BlogRepository.java
@@ -6,6 +6,8 @@ import com.blinkfox.fenix.provider.BlogSqlInfoProvider;
 import com.blinkfox.fenix.vo.UserBlogDto;
 import com.blinkfox.fenix.vo.UserBlogInfo;
 import com.blinkfox.fenix.vo.UserBlogProjection;
+import java.util.List;
+import java.util.Map;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -13,8 +15,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-import java.util.Map;
 
 /**
  * 博客数据的库持久化类.
