a5ba13557e8ffcf60c19bbb2c1538a880d38f6ea
niejianjun <niejianjun111@126.com>
2021-02-04 14:35:04 +0800

    Update FenixResultTransformer.java

--- src/main/java/com/blinkfox/fenix/jpa/FenixResultTransformer.java
+++ src/main/java/com/blinkfox/fenix/jpa/FenixResultTransformer.java
@@ -1,17 +1,25 @@
 package com.blinkfox.fenix.jpa;
 
+import com.blinkfox.fenix.consts.Const;
 import com.blinkfox.fenix.exception.FenixException;
 import com.blinkfox.fenix.helper.StringHelper;
-
 import java.beans.PropertyDescriptor;
+import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
+import java.math.BigDecimal;
 import java.sql.Blob;
 import java.sql.Clob;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
+import org.apache.commons.lang3.ArrayUtils;
+import org.hibernate.HibernateException;
+import org.hibernate.engine.spi.SessionFactoryImplementor;
+import org.hibernate.property.access.internal.PropertyAccessStrategyBasicImpl;
+import org.hibernate.property.access.internal.PropertyAccessStrategyChainedImpl;
+import org.hibernate.property.access.internal.PropertyAccessStrategyFieldImpl;
+import org.hibernate.property.access.internal.PropertyAccessStrategyMapImpl;
+import org.hibernate.property.access.spi.PropertyAccessStrategy;
+import org.hibernate.property.access.spi.Setter;
 import org.hibernate.transform.ResultTransformer;
 import org.hibernate.type.BlobType;
 import org.hibernate.type.descriptor.java.DataHelper;
@@ -44,12 +52,16 @@ public class FenixResultTransformer<T> implements ResultTransformer {
     /**
      * 要转换类型的 class 实例.
      */
-    private Class<T> resultClass;
+    private final Class<T> resultClass;
 
     /**
      * 返回结果各属性字段名称及对应的对象间的映射关系 Map.
      */
     private final transient Map<String, PropertyDescriptor> fieldMap;
+
+    private boolean isInitialized;
+    private String[] aliases;
+    private Setter[] setters;
 
     static {
         // 添加一些默认的 ConversionService.
@@ -69,6 +81,7 @@ public class FenixResultTransformer<T> implements ResultTransformer {
     public FenixResultTransformer(Class<T> resultClass) {
         Assert.notNull(resultClass, "【Fenix 异常】resultClass cannot be null.");
         this.resultClass = resultClass;
+        this.isInitialized = false;
 
         // 将返回结果类中的所有属性保存到 Map 中，便于后续快速获取和判断.
         this.fieldMap = new HashMap<>();
@@ -81,45 +94,114 @@ public class FenixResultTransformer<T> implements ResultTransformer {
     /**
      * 将每行的“元组”数据转换为对应的 {@link #resultClass} 的结果类型.
      *
-     * @param tuple   元组数据
+     * @param tuple 元组数据
      * @param aliases 元组数据的别名数组
      * @return 转换后的 {@link #resultClass} 的结果类型
      */
     @Override
     public Object transformTuple(Object[] tuple, String[] aliases) {
-        // 构造结果实例.
-        T resultObject;
         try {
-            resultObject = this.resultClass.getDeclaredConstructor().newInstance();
-        } catch (InstantiationException | IllegalAccessException
-                | NoSuchMethodException | InvocationTargetException e) {
-            throw new FenixException("实例化【" + this.resultClass + "】类出错，请检查该类是否包含可公开访问的无参构造方法！", e);
+            if (!this.isInitialized) {
+                this.initialize(aliases);
+            } else {
+                this.check(aliases);
+            }
+
+            Object result = this.resultClass.newInstance();
+
+            for (int e = 0; e < aliases.length; ++e) {
+                if (this.setters[e] != null) {
+                    if (tuple[e] != null
+                            && this.setters[e].getMethod() != null
+                            && this.setters[e].getMethod().getParameterTypes().length > 0
+                            && "java.lang.Character".equals(this.setters[e].getMethod().getParameterTypes()[0].getName())) {
+                        this.setters[e].set(result, tuple[e].toString().charAt(0), (SessionFactoryImplementor) null);
+                    } else if (tuple[e] != null
+                            && this.setters[e].getMethod() != null
+                            && this.setters[e].getMethod().getParameterTypes().length > 0
+                            && "java.lang.Long".equals(this.setters[e].getMethod().getParameterTypes()[0].getName())) {
+                        this.setters[e].set(result, Long.valueOf(tuple[e].toString()), (SessionFactoryImplementor) null);
+                    } else if (tuple[e] != null
+                            && this.setters[e].getMethod() != null
+                            && this.setters[e].getMethod().getParameterTypes().length > 0
+                            && "java.lang.Integer".equals(this.setters[e].getMethod().getParameterTypes()[0].getName())) {
+                        this.setters[e].set(result, Integer.valueOf(tuple[e].toString()), (SessionFactoryImplementor) null);
+                    } else if (tuple[e] != null
+                            && this.setters[e].getMethod() != null
+                            && this.setters[e].getMethod().getParameterTypes().length > 0
+                            && "java.lang.Float".equals(this.setters[e].getMethod().getParameterTypes()[0].getName())) {
+                        this.setters[e].set(result, Float.valueOf(tuple[e].toString()), (SessionFactoryImplementor) null);
+                    } else if (tuple[e] != null
+                            && this.setters[e].getMethod() != null
+                            && this.setters[e].getMethod().getParameterTypes().length > 0
+                            && "java.math.BigDecimal".equals(this.setters[e].getMethod().getParameterTypes()[0].getName())) {
+                        this.setters[e].set(result, new BigDecimal(tuple[e].toString()), (SessionFactoryImplementor) null);
+                    } else if (tuple[e] != null
+                            && this.setters[e].getMethod() != null
+                            && this.setters[e].getMethod().getParameterTypes().length > 0
+                            && "java.lang.Boolean".equals(this.setters[e].getMethod().getParameterTypes()[0].getName())) {
+                        if (Const.BOOLEAN_STRING_TRUE.equalsIgnoreCase(tuple[e].toString())
+                                || Const.FLAG_STRING_TRUE.equals(tuple[e].toString())) {
+                            this.setters[e].set(result, Boolean.TRUE, (SessionFactoryImplementor) null);
+                        } else if (Const.BOOLEAN_STRING_FALSE.equalsIgnoreCase(tuple[e].toString())
+                                || Const.FLAG_STRING_FALSE.equals(tuple[e].toString())) {
+                            this.setters[e].set(result, Boolean.FALSE, (SessionFactoryImplementor) null);
+                        }
+                    } else if (tuple[e] != null
+                            && (Const.BOOLEAN_STRING_TRUE.equals(tuple[e].toString()) || Const.BOOLEAN_STRING_FALSE.equals(tuple[e].toString()))
+                            && this.setters[e].getMethod() != null
+                            && this.setters[e].getMethod().getParameterTypes().length > 0
+                            && "java.lang.Character".equals(this.setters[e].getMethod().getParameterTypes()[0].getName())) {
+                        this.setters[e].set(result, Boolean.valueOf(tuple[e].toString()), (SessionFactoryImplementor) null);
+                    } else {
+                        this.setters[e].set(result, tuple[e], (SessionFactoryImplementor) null);
+                    }
+                }
+            }
+            return result;
+        } catch (InstantiationException | IllegalAccessException var5) {
+            throw new HibernateException("Could not instantiate resultclass: " + this.resultClass.getName());
+        }
+    }
+
+    private void initialize(String[] aliases) {
+        PropertyAccessStrategyChainedImpl propertyAccessStrategy = new PropertyAccessStrategyChainedImpl(new PropertyAccessStrategy[]{PropertyAccessStrategyBasicImpl.INSTANCE, PropertyAccessStrategyFieldImpl.INSTANCE, PropertyAccessStrategyMapImpl.INSTANCE});
+        this.aliases = new String[aliases.length];
+        this.setters = new Setter[aliases.length];
+
+        Field[] fields = resultClass.getDeclaredFields();
+
+        Class<?> tempClass = resultClass.getSuperclass();
+        //获取当前类父类的属性
+        while (tempClass != null && !tempClass.getName().toLowerCase().equals("java.lang.object")) {
+            Field[] baseFields = tempClass.getDeclaredFields();
+            fields = ArrayUtils.addAll(baseFields, fields);
+            tempClass = tempClass.getSuperclass(); //得到父类,然后赋给自己
+        }
+        if (fields == null || fields.length == 0) {
+            throw new RuntimeException(resultClass.getName() + " property is null");
+        }
+        for (int i = 0; i < aliases.length; ++i) {
+            String alias = aliases[i];
+            if (alias != null) {
+                this.aliases[i] = alias;
+                for (Field field : fields) {
+                    if (field.getName().equalsIgnoreCase(alias.replaceAll("_", ""))) {
+                        this.setters[i] = propertyAccessStrategy.buildPropertyAccess(this.resultClass, field.getName()).getSetter();
+                        break;
+                    }
+                }
+            }
         }
 
-        BeanWrapper beanWrapper = PropertyAccessorFactory.forBeanPropertyAccess(resultObject);
-        beanWrapper.setConversionService(conversionService);
+        this.isInitialized = true;
 
-        // 遍历设置各个属性对应的值.
-        for (int i = 0, len = aliases.length; i < len; i++) {
-            String column = aliases[i];
-            if (column == null || StringHelper.isBlank(column)) {
-                throw new FenixException("【Fenix 异常】要映射为【" + this.resultClass + "】实体的查询结果列为空，"
-                        + "请检查并保证每一个查询结果列都必须用【as】后加“别名”的方式！");
-            }
+    }
 
-            PropertyDescriptor propDescriptor = this.fieldMap.get(column.replaceAll(" ", "").toLowerCase());
-            if (propDescriptor == null) {
-                continue;
-            }
-
-            try {
-                beanWrapper.setPropertyValue(propDescriptor.getName(), tuple[i]);
-            } catch (NotWritablePropertyException | TypeMismatchException e) {
-                throw new FenixException("【Fenix 异常】设置字段【" + column + "】的值到属性【"
-                        + propDescriptor.getName() + "】中出错，请检查该字段或属性是否存在或者可公开访问！", e);
-            }
+    private void check(String[] aliases) {
+        if (!Arrays.equals(aliases, this.aliases)) {
+            throw new IllegalStateException("aliases are different from what is cached; aliases=" + Arrays.asList(aliases) + " cached=" + Arrays.asList(this.aliases));
         }
-        return resultObject;
     }
 
     /**
